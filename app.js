adbee209ee75e9647eb8d4c4094ccdd6efd98978
Luiz Cunha <luiz9495@gmail.com>
2019-05-09 16:05:34 +0900

    Assignment 2: MongoDB

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 
-const Dishes = require('./models/dishes');
+//const Dishes = require('./models/dishes');
 
 const url ='mongodb://localhost:27017/conFusion';
 //const connect = mongoose.connect(url, { useNewUrlParser: true });
