3ce79243f9d21ba95ee01f21e24db804848eddaf
Luiz Cunha <luiz9495@gmail.com>
2019-05-14 16:21:45 +0900

    Passport JWT

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var session = require('express-session'); // express-session
 var FileStore = require('session-file-store')(session); // filestore
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,10 +16,11 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
+// !!! mongoose.Promise = require('bluebird');
 
-//const Dishes = require('./models/dishes');
+// const url ='mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 
-const url ='mongodb://localhost:27017/conFusion';
 //const connect = mongoose.connect(url, { useNewUrlParser: true });
 const connect = mongoose.connect(url);
 
@@ -39,36 +41,36 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321')); // use signed cookies
 
 // Setup session
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+// --- app.use(session({
+//  name: 'session-id',
+//  secret: '12345-67890-09876-54321',
+//  saveUninitialized: false,
+//  resave: false,
+//  store: new FileStore()
+//}));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// --- app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// From this step: go through authorization phase before Middleware can be accessed (with signed cookies to store auth)
-function auth (req, res, next) {
+// --- From this step: go through authorization phase before Middleware can be accessed (with signed cookies to store auth)
+//function auth (req, res, next) {
 //  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-    next();
-  }
-}
+//
+//  if (!req.user) {
+//    var err = new Error('You are not authenticated!');
+//    err.status = 403;
+//    next(err);
+//  }
+//  else {
+//    next();
+//  }
+//}
 // end Auth + Cookies
-
-app.use(auth);
+//
+//app.use(auth);
 //
 
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+// !!! const
+var authenticate = require('../authenticate');
 
 const Dishes = require('../models/dishes');
 
@@ -19,7 +21,7 @@ dishRouter.route('/')
       }, (err) => next(err))
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log('Dish Created ', dish);
@@ -29,11 +31,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
       res.statusCode = 403;
       res.end('PUT operation not supported on /dishes');
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.deleteMany({})
     .then((resp) => {
         res.statusCode = 200;
@@ -55,11 +57,11 @@ dishRouter
       }, (err) => next(err))
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId);
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
     }, { new: true })
@@ -70,7 +72,7 @@ dishRouter
     }, (err) => next(err))
     .catch((err) => next(err));
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp) => {
         res.statusCode = 200;
@@ -98,7 +100,7 @@ dishRouter.route('/:dishId/comments')
     }, (err) => next(err))
     .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
       if (dish != null) {
@@ -118,11 +120,11 @@ dishRouter.route('/:dishId/comments')
     }, (err) => next(err))
     .catch((err) => next(err));
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
       res.statusCode = 403;
       res.end('PUT operation not supported on /dishes/' + req.params.dishId + '/comments');
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
       if (dish != null) {
@@ -170,12 +172,12 @@ dishRouter.route('/:dishId/comments/:commentId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId
         + '/comments/' + req.params.commentId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -205,7 +207,7 @@ dishRouter.route('/:dishId/comments/:commentId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
